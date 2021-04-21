580cda1d9e548053b590c0c1c972b8130abe3eb5
ToJIKa4-UA <81953303+ToJIKa4-UA@users.noreply.github.com>
2021-04-21 18:18:26 +0300

    tajem
    some one landing

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.header-burger').click(function(event) {
+        $('.heaer-logo,.header-burger,.header-container,.header-nav,.nav-items,.nav-links').toggleClass('active');
+    });
+});
+
+$(document).on('click', ".arrow-left",function(){ 
+	let carusel = $(this).parents('.carousel');
+	right_carusel(carusel);
+	return false;
+});
+//Обработка клика на стрелку влево
+$(document).on('click',".arrow-right",function(){ 
+	var carusel = $(this).parents('.carousel');
+	left_carusel(carusel);
+	return false;
+});
+function left_carusel(carusel){
+   var block_width = $(carusel).find('.carousel-block').outerWidth();
+   $(carusel).find(".carousel-items .carousel-block").eq(-1).clone().prependTo($(carusel).find(".carousel-items")); 
+   $(carusel).find(".carousel-items").css({"left":"-"+block_width+"px"});
+   $(carusel).find(".carousel-items .carousel-block").eq(-1).remove();    
+   $(carusel).find(".carousel-items").animate({left: "0px"}, 200); 
+   
+}
+function right_carusel(carusel){
+   var block_width = $(carusel).find('.carousel-block').outerWidth();
+   $(carusel).find(".carousel-items").animate({left: "-"+ block_width +"px"}, 200, function(){
+	  $(carusel).find(".carousel-items .carousel-block").eq(0).clone().appendTo($(carusel).find(".carousel-items")); 
+      $(carusel).find(".carousel-items .carousel-block").eq(0).remove(); 
+      $(carusel).find(".carousel-items").css({"left":"0px"}); 
+   }); 
+}
